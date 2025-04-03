9bceaa507faf9b41da624175f673ba0527334043
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-04-03 17:38:37 +0200

    Character finalizado

--- src/personajes/Character.java
+++ src/personajes/Character.java
@@ -1,5 +1,7 @@
 package personajes;
 
+import java.util.Random;
+
 public abstract class Character {
 
     private String id, name; //el id ha de generarse aleatoriamente
@@ -9,9 +11,55 @@ public abstract class Character {
     private boolean isAlive = true;
 
     public Character(String name, int hp) {
+
+        //genero un string aleatorio
+        id = generateRandomString();
+
         this.name = name;
         this.hp = hp;
     }
 
+    private String generateRandomString(){
+        int longitud = 4; // Longitud del String aleatorio
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder stringAleatorio = new StringBuilder();
+        var random = new Random();
 
+        for (int i = 0; i < longitud; i++) {
+            int index = random.nextInt(caracteres.length());
+            stringAleatorio.append(caracteres.charAt(index));
+        }
+
+        return stringAleatorio.toString();
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
