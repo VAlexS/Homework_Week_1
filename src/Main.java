b4e2c9a72f0066e039082de1dc3c38c69b425383
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-04-07 14:56:49 +0200

    Jugadores añadidos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import characters.Character;
+import characters.Warrior;
+import characters.Wizard;
+
+import java.util.Random;
+
 public class Main {
+
+    public static int generateRandomNumber(int min, int max){
+        Random random = new Random();
+
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Character warrior1 = new Warrior("Warrior 1", generateRandomNumber(100, 200), generateRandomNumber(10, 50), generateRandomNumber(1, 10));
+
+        Character wizzard1 = new Wizard("Wizzard 1", generateRandomNumber(50, 100), generateRandomNumber(10, 50), generateRandomNumber(1, 50));
+
+        
     }
 }
